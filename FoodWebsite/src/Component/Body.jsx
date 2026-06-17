aae7acf21f10d6ab317d4c68cf78d1382b1d3be1
diwaker321 <diwakeracadmic@gmail.com>
2026-06-17 09:42:38 +0530

    code has been pushed

--- FoodWebsite/src/Component/Body.jsx
+++ FoodWebsite/src/Component/Body.jsx
@@ -4,44 +4,108 @@ import FoodCard from "./FoodCard";
 import Shimmer from "./Shimmer";
 const Body = () => {
   let [data, setdata] = useState([]);
-  // let [data, setdata] = useState([]);
-  
+  // let [searchvalue, setSearchValue] = useState("");
+  let [ searchinput , setSearchInput] = useState("")
+  let [filterdata , setfilterdata] = useState([])
 
-  // if(data?.length==0){
-  //   return <Shimmer/>
-  // }
+  const handleSearchChange = (event) => {
+    setSearchValue(event.target.value);
+    console.log("searchvalue: ", searchvalue);
+  };
 
+  const handleSearch = ()=>{
+    let val = data.filter((res)=> res.strMeal.toLowerCase().includes(searchvalue.toLowerCase()))
+    console.log('val: ', val);
+    setdata(val)
 
+  }
 
-
-  const getdata = async()=>{
-    try{
-      const res = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert')
-      const data = await res.json()
+  const getdata = async () => {
+    try {
+      const res = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert",
+      );
+      const data = await res.json();
       // console.log('data: ', data.meals);
-      setdata(data.meals)
-      console.log('data: ', data.meals);
-
-    }catch(err){
+      setdata(data.meals);
+    } catch (err) {
       console.log(err);
     }
+  };
+
+  // const getSearchdata = async () => {
+  //   try {
+  //     const res = await fetch(
+  //       `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchvalue}`,
+  //     );
+
+  //     const data = await res.json();
+  //     // console.log('data: ', data.meals);
+  //     console.log("data: ", data);
+  //     setdata(data.meals || []);
+  //   } catch (err) {}
+  // };
+
+  // useEffect(() => {
+  //   if (searchvalue.trim() === "") {
+  //     getdata();
+  //   }
+  // }, [searchvalue]);
+
+  useEffect(() => {
+    getdata();
+  }, []);
+
+  const handleClick = ()=>{
+    console.log('clicked');
+    
+    
   }
-  
+  const getSearchData = async()=>{
+    try{
+      const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchinput}`)
+      const data = await res.json()
+      console.log('data: ', data?.meals);
+      setdata(data?.meals)
+    }catch(err){
+      console.log('err: ', err);
+    }
+
+  }
+
+  const handleSearchchange = (event)=>{
+    setSearchInput(event.target.value)
+    console.log('searchinput: ', searchinput);
+    // getSearchData()
+  }
+
   useEffect(()=>{
-    getdata()
-  },[])
+    console.log(searchinput.length);
+    if(searchinput.length===0){
+      getdata()
+    }else{
+      getSearchData()
+    }
+  },[searchinput])
 
-  console.log('data.length: ', data.length);
-
-  if(data.length===0){
-    return <Shimmer/>
+  if (data?.length === 0) {
+    return <Shimmer />;
   }
-  
 
   return (
     <div className="body_section">
       <div className="searchbarsection">
-        <input type="search" placeholder="enter your food" />
+        {/* <input
+          type="search"
+          placeholder="enter your food"
+          value={searchvalue}
+          onChange={handleSearchChange}
+        /> */}
+
+        {/* <button onClick={handleSearch}>Search</button> */}
+
+        <input type="search" placeholder="Enter your dish" value={searchinput} onChange={handleSearchchange} />
+        <button onClick={handleClick}>Search</button>
       </div>
       <div className="foodContainer">
         {data?.map((res) => {
